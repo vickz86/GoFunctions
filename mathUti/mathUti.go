76dede22c10ffa40fc24941c5126b9cf156896ed
victor chassaigne <vicchassaigne@hotmail.com>
2024-07-09 11:43:18 -0400

    add mathUti PercVarWithLastAndOther function

--- mathUti/mathUti.go
+++ mathUti/mathUti.go
@@ -1,6 +1,9 @@
 package mathuti
 
-import "math"
+import (
+	"errors"
+	"math"
+)
 
 // PercentageVariation : get the percentage variation between 2 float
 func PercentageVariation(float1, float2 float64) float64 {
@@ -14,5 +17,33 @@ func PercentageVariation(float1, float2 float64) float64 {
 	final := div * 100
 
 	return final
+
+}
+
+// PercVarWithLastAndOther : get percentage variation between last value of slice of float and other
+// 0 to compare with first of slice , 1 to walk backward
+// return percentage variation
+func PercVarWithLastAndOther(slicefloat []float64, ToCompareWith int) (float64, error) {
+	// check for error
+	if ToCompareWith > len(slicefloat) {
+		return 0.0, errors.New("error,CompareWith int is too big")
+	}
+
+	//get last value
+	lastVal := slicefloat[len(slicefloat)-1]
+
+	//get value to compare with
+	var CompVal float64
+	if ToCompareWith == 0 {
+		CompVal = slicefloat[0]
+	} else {
+		CompVal = slicefloat[len(slicefloat)-1-ToCompareWith]
+	}
+
+	//Create the variation using the PercentageVariation function
+	difVal := PercentageVariation(CompVal, lastVal)
+
+	//return the value
+	return difVal, nil
 
 }
--- mathUti/mathUti_test.go
+++ mathUti/mathUti_test.go
@@ -6,13 +6,26 @@ import (
 )
 
 func TestPercentageVariation(t *testing.T) {
-	want := 10.0
+	want := 20.0
 	got := PercentageVariation(50.0, 60.0)
 
 	if want == got {
 		fmt.Println("PercentageVariation has passed")
 	} else {
-		fmt.Printf("error PercentageVariation ,want %.1f and got %.1f", want, got)
+		fmt.Printf("error PercentageVariation ,want %.1f and got %.1f\n", want, got)
+	}
+
+}
+
+func TestPercVarWithLastAndOther(t *testing.T) {
+	var sliceTOTest = []float64{5.0, 10.0, 15.0, 20.0}
+
+	want := 300.0
+	got, _ := PercVarWithLastAndOther(sliceTOTest, 0)
+
+	if want != got {
+		fmt.Printf("error PercVarWithLastAndOther, got %.1f and want %1.f\n", want, got)
+
 	}
 
 }
